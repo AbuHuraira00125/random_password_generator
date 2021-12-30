a7019b39a53b980ab81acd5b7359b8ac46a3b941
AbuHuraira00125 <92776480+AbuHuraira00125@users.noreply.github.com>
2021-12-29 21:34:31 -0800

    Changes

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,8 +2,72 @@ import 'package:flutter/material.dart';
 import 'package:random_password_generator/random_password_generator.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    title: "Simple Math app for Kids",
+    home: homePage(),
+  ));
 }
+
+class homePage extends StatelessWidget {
+  const homePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Abu Huraira Password Generator app"),
+      ),
+      drawer: Drawer(
+
+
+
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              padding: EdgeInsets.only(top: 60, left: 20),
+              child: Text('AH Password Generator app ' ,  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25 , color: Colors.white),),
+            ),
+            ListTile(
+              title: const Text('Simple' ,style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => MyApp()));
+              },
+            ),
+            ListTile(
+              title: const Text('Hard',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => MyApp1()));
+              },
+            ),
+          ],
+        ),
+
+      ),
+
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class MyApp extends StatefulWidget {
   @override
@@ -53,11 +117,11 @@ class _MyAppState extends State<MyApp> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                checkBox('Upper Case', (bool value) {
+                checkBox('Two Upper Case Letters', (bool value) {
                   _isWithUppercase = value;
                   setState(() {});
                 }, _isWithUppercase),
-                checkBox('Lower Case', (bool value) {
+                checkBox( 'Two Lower Case', (bool value) {
                   _isWithLetters = value;
                   setState(() {});
                 }, _isWithLetters)
@@ -69,11 +133,11 @@ class _MyAppState extends State<MyApp> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                checkBox('Symbols', (bool value) {
+                checkBox('Two Symbols', (bool value) {
                   _isWithSpecial = value;
                   setState(() {});
                 }, _isWithSpecial),
-                checkBox('Numbers', (bool value) {
+                checkBox('Two Numbers', (bool value) {
                   _isWithNumbers = value;
                   setState(() {});
                 }, _isWithNumbers)
@@ -93,7 +157,7 @@ class _MyAppState extends State<MyApp> {
                   ),
                   filled: true,
                   fillColor: Colors.grey[300],
-                  labelText: 'Enter Length',
+                  labelText: 'Enter Length of simple password',
                   labelStyle: TextStyle(color: Colors.blue),
                 ),
                 keyboardType: TextInputType.number,
@@ -136,7 +200,7 @@ class _MyAppState extends State<MyApp> {
                   child: Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Text(
-                      'Generator Password',
+                      'Generate Simple Password',
                       style: TextStyle(color: Colors.white, fontSize: 20),
                     ),
                   ),
@@ -172,3 +236,251 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+class MyApp1 extends StatefulWidget {
+  @override
+  _MyApp1State createState() => _MyApp1State();
+}
+
+class _MyApp1State extends State<MyApp1> {
+  bool _isWithLetters = true;
+  bool _isWithUppercase = false;
+  bool _isWithNumbers = false;
+  bool _isWithSpecial = false;
+  double _numberCharPassword = 8;
+  String newPassword = '';
+  Color _color = Colors.blue;
+  String isOk = '';
+  TextEditingController _passwordLength = TextEditingController();
+  final password = RandomPasswordGenerator();
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  checkBox(String name, Function onTap, bool value) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(name),
+        Checkbox(value: value, onChanged: onTap),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Random Password Generator'),
+        ),
+        body: Center(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    checkBox('Upper Case', (bool value) {
+                      _isWithUppercase = value;
+                      setState(() {});
+                    }, _isWithUppercase),
+                    checkBox('Lower Case', (bool value) {
+                      _isWithLetters = value;
+                      setState(() {});
+                    }, _isWithLetters)
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    checkBox('Symbols', (bool value) {
+                      _isWithSpecial = value;
+                      setState(() {});
+                    }, _isWithSpecial),
+                    checkBox('Numbers', (bool value) {
+                      _isWithNumbers = value;
+                      setState(() {});
+                    }, _isWithNumbers)
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: TextField(
+                    controller: _passwordLength,
+                    decoration: InputDecoration(
+                      border: new OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(25.0),
+                        borderSide: new BorderSide(),
+                      ),
+                      filled: true,
+                      fillColor: Colors.grey[300],
+                      labelText: 'Enter Number of symbols',
+                      labelStyle: TextStyle(color: Colors.blue),
+                    ),
+                    keyboardType: TextInputType.number,
+                  ),
+                ),
+
+                SizedBox(
+                  height: 20,
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: TextField(
+                    controller: _passwordLength,
+                    decoration: InputDecoration(
+                      border: new OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(25.0),
+                        borderSide: new BorderSide(),
+                      ),
+                      filled: true,
+                      fillColor: Colors.grey[300],
+                      labelText: 'Enter Number of characters',
+                      labelStyle: TextStyle(color: Colors.blue),
+                    ),
+                    keyboardType: TextInputType.number,
+                  ),
+                ),
+
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: TextField(
+                    controller: _passwordLength,
+                    decoration: InputDecoration(
+                      border: new OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(25.0),
+                        borderSide: new BorderSide(),
+                      ),
+                      filled: true,
+                      fillColor: Colors.grey[300],
+                      labelText: 'Enter Number of lower case',
+                      labelStyle: TextStyle(color: Colors.blue),
+                    ),
+                    keyboardType: TextInputType.number,
+                  ),
+                ),
+
+                SizedBox(
+                  height: 20,
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: TextField(
+                    controller: _passwordLength,
+                    decoration: InputDecoration(
+                      border: new OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(25.0),
+                        borderSide: new BorderSide(),
+                      ),
+                      filled: true,
+                      fillColor: Colors.grey[300],
+                      labelText: 'Enter Number of upper case',
+                      labelStyle: TextStyle(color: Colors.blue),
+                    ),
+                    keyboardType: TextInputType.number,
+                  ),
+                ),
+
+
+                SizedBox(
+                  height: 20,
+                ),
+                FlatButton(
+                    onPressed: () {
+                      if (_passwordLength.text.trim().isNotEmpty)
+                        _numberCharPassword =
+                            double.parse(_passwordLength.text.trim());
+
+                      newPassword = password.randomPassword(
+                          letters: _isWithLetters,
+                          numbers: _isWithNumbers,
+                          passwordLength: _numberCharPassword,
+                          specialChar: _isWithSpecial,
+                          uppercase: _isWithUppercase);
+
+                      print(newPassword);
+                      double passwordstrength =
+                      password.checkPassword(password: newPassword);
+                      if (passwordstrength < 0.3) {
+                        _color = Colors.red;
+                        isOk = 'This password is weak!';
+                      } else if (passwordstrength < 0.7) {
+                        _color = Colors.blue;
+                        isOk = 'This password is Good';
+                      } else {
+                        _color = Colors.green;
+                        isOk = 'This passsword is Strong';
+                      }
+
+                      setState(() {});
+                    },
+                    child: Container(
+                      color: Colors.red,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'Generate Hard Password',
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                      ),
+                    )),
+                SizedBox(
+                  height: 10,
+                ),
+                if (newPassword.isNotEmpty && newPassword != null)
+                  Center(
+                      child: SingleChildScrollView(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            isOk,
+                            style: TextStyle(color: _color, fontSize: 25),
+                          ),
+                        ),
+                      )),
+                if (newPassword.isNotEmpty && newPassword != null)
+                  Center(
+                      child: SingleChildScrollView(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            newPassword,
+                            style: TextStyle(color: _color, fontSize: 25),
+                          ),
+                        ),
+                      ))
+              ],
+            )),
+      ),
+    );
+  }
+}
+
+
+
+
